118d6d851d0eb4f6811b5b08023af518c649713a
s2458588 <ricardo.jung@mailbox.org>
2025-08-18 20:07:11 +0200

    tests2.rs

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -10,14 +10,16 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::{any::Any, error::Error};
+
     use super::*;
 
     #[test]
     fn you_can_assert_eq() {
         // TODO: Test the function `power_of_2` with some values.
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
+        assert_eq!(power_of_2(1), 2, "{}", "" );
+        assert_eq!(power_of_2(0), 1, "{}", "" );
+        assert_eq!(power_of_2(2), 4);
+        assert_eq!(power_of_2(12), 4096);
     }
 }
