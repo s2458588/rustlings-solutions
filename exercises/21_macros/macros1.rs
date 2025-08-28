bba186ab92421fe70902fa611f0c56006525a3fc
s2458588 <ricardo.jung@mailbox.org>
2025-08-28 15:33:59 +0200

    macros + clippy

--- exercises/21_macros/macros1.rs
+++ exercises/21_macros/macros1.rs
@@ -6,5 +6,4 @@ macro_rules! my_macro {
 
 fn main() {
     // TODO: Fix the macro call.
-    my_macro();
-}
+    my_macro!(); }
--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -1,9 +1,16 @@
+
+// TODO: Fix the compiler error by moving the whole definition of this macro.
+macro_rules! my_macro {
+    () => {
+        println!("Check out my macro!");
+    };
+}
 fn main() {
     my_macro!();
 }
 
 // TODO: Fix the compiler error by moving the whole definition of this macro.
-macro_rules! my_macro {
+macro_rules! my_macro2 {
     () => {
         println!("Check out my macro!");
     };
