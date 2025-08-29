c1e03fad53f5024160f5de962677198d1f8fc20d
s2458588 <ricardo.jung@mailbox.org>
2025-08-29 20:01:52 +0200

    progress on from_into.rs

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -3,6 +3,8 @@
 // You can read more about it in the documentation:
 // https://doc.rust-lang.org/std/convert/trait.From.html
 
+use core::slice;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -20,7 +22,7 @@ impl Default for Person {
     }
 }
 
-// TODO: Complete this `From` implementation to be able to parse a `Person`
+// TODO: Complete this `From` implementation t<F9>o be able to parse a `Person`
 // out of a string in the form of "Mark,20".
 // Note that you'll need to parse the age component into a `u8` with something
 // like `"4".parse::<u8>()`.
@@ -34,7 +36,25 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+
+        let sliced: Vec<&str> = s.split(",").collect();
+        if sliced.len() != 2 {
+            if sliced.2 == "" {
+                Person::default()
+            }
+            else {
+            Self {
+                    name: sliced.0,
+                    age: sliced.1,
+                }
+            }
+        }
+        else {
+        Person::default()
+        }
+        // function body end
+        }
 }
 
 fn main() {
